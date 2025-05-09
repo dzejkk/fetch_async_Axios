ba698866695a3d22cd0599d1e20a7e40af0d3757
Jakub Kontra <kontrajakub2@gmail.com>
2025-05-09 12:15:57 +0000

    added axios

--- readme.md
+++ readme.md
@@ -1,4 +1,14 @@
+#  Rozne typy  tahania dat s  API
 
+
+1. basic fetch with then()
+2. asynch await aproach + modules
+3. axios aproach
+
+
+
+Hint
+***
  Using parameter destructuring is a clean and elegant solution. It allows you to directly extract the parsedData property from the incoming object right in the function signature.
 
 Here's what's happening:
@@ -10,3 +20,4 @@ property inside the function, we're using destructuring { parsedData }
 in the function parameter itself
 This automatically extracts the parsedData property from the object and 
 makes it available as a variable inside the function
+***
--- src/main.js
+++ src/main.js
@@ -36,30 +36,59 @@ import "./style.css";
 
 /* async await + module aproach */
 
-import { fetchData } from "./fetchData";
+// import { fetchData } from "./fetchData";
 
-fetchData()
-  .then(renderWeather)
-  .catch((e) => {
-    console.error(e);
-    alert("Error getting weather.");
+// fetchData()
+//   .then(renderWeather)
+//   .catch((e) => {
+//     console.error(e);
+//     alert("Error getting weather.");
+//   });
+
+// function renderWeather({ parsedData }) {
+//   const container = document.querySelector("#app");
+
+//   //console.log(parsedData);
+//   container.innerHTML = parsedData
+//     .map((item, index) => {
+//       const { firstName: name, emailAddress: mail } = item;
+//       return `
+//               <div id="${index}" class="card">
+//                   <div class="name-container">
+//                     <p>${index}</p>
+//                     <h1>${name}</h1>
+//                   </div>
+//                 <h2>${mail}</h2>
+//               </div>`;
+//     })
+//     .join("");
+// }
+
+/* AXIOS */
+
+import axios from "axios";
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+async function getUsers() {
+  try {
+    const response = await axios.get(API_URL);
+    console.log(response);
+
+    return {
+      parsedData: parseAPI(response),
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getUsers();
+
+function parseAPI(dataAPI) {
+  const { data } = dataAPI;
+  console.log(data);
+
+  data.forEach((user) => {
+    console.log(user.name);
   });
-
-function renderWeather({ parsedData }) {
-  const container = document.querySelector("#app");
-
-  //console.log(parsedData);
-  container.innerHTML = parsedData
-    .map((item, index) => {
-      const { firstName: name, emailAddress: mail } = item;
-      return `
-              <div id="${index}" class="card">
-                  <div class="name-container">
-                    <p>${index}</p>
-                    <h1>${name}</h1>
-                  </div>
-                <h2>${mail}</h2>
-              </div>`;
-    })
-    .join("");
 }
